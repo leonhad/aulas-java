c6b1fb8d670c0d814dac88146c3d22b3dc6f1fdd
Leonardo Alves da Costa <leonhad@gmail.com>
2020-02-21 08:28:01 -0300

    Adicionado método de alteração e exclusão.

--- SpringWeb/Aula 1/aula1-final/src/main/java/com/example/aula1/controller/UsuarioController.java
+++ SpringWeb/Aula 1/aula1-final/src/main/java/com/example/aula1/controller/UsuarioController.java
@@ -4,7 +4,11 @@ import com.example.aula1.entity.UsuarioEntity;
 import com.example.aula1.repository.UsuarioRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,18 +21,28 @@ public class UsuarioController {
     @Autowired
     private UsuarioRepository usuarioRepository;
     
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public List<UsuarioEntity> listAll() {
         return usuarioRepository.findAll();
     }
     
-    @RequestMapping(method = RequestMethod.POST)
-    public UsuarioEntity incluir(@RequestBody UsuarioEntity usuario) {
+    @PostMapping
+    public UsuarioEntity create(@RequestBody UsuarioEntity usuario) {
         return usuarioRepository.save(usuario);
     }
     
-    @RequestMapping(path = "/{id}")
+    @GetMapping(path = "/{id}")
     public UsuarioEntity get(@PathVariable Long id) {
         return usuarioRepository.getOne(id);
+    }
+    
+    @PutMapping
+    public UsuarioEntity update(@RequestBody UsuarioEntity usuario) {
+        return usuarioRepository.save(usuario);
+    }
+    
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable Long id) {
+        usuarioRepository.deleteById(id);
     }
 }
