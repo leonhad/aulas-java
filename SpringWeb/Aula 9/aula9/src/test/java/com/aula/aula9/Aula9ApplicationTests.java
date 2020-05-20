2a5f24864d0c14563a8b1cde45ab9f39bbc9d415
Leonardo Costa <leonhad@gmail.com>
2020-05-20 19:16:07 -0300

    Adicionada a aula 9.

---
+++ SpringWeb/Aula 9/aula9/src/test/java/com/aula/aula9/Aula9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula.aula9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
