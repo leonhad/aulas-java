daa8e728c52620a2ebf62eb28858aad376e0fc80
Leonardo Costa <leonhad@gmail.com>
2020-05-20 22:30:19 -0300

    Finalizado o código da aula 9.

---
+++ SpringWeb/Aula 9/aula9-final/src/main/java/com/aula/aula9/controller/AlunoController.java
@@ -0,0 +1,33 @@
+package com.aula.aula9.controller;
+
+import com.aula.aula9.controller.retorno.Aluno;
+import com.aula.aula9.entity.AlunoEntity;
+import com.aula.aula9.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @GetMapping
+    public List<Aluno> findAll() {
+        final List<AlunoEntity> lista = alunoRepository.findAll();
+        //return lista.stream().map(Aluno::new).collect(Collectors.toList());
+
+        ArrayList<Aluno> alunos = new ArrayList<>();
+        for (AlunoEntity alunoEntity : lista) {
+            alunos.add(new Aluno(alunoEntity));
+        }
+        return alunos;
+    }
+}
---
+++ SpringWeb/Aula 9/aula9-final/src/main/java/com/aula/aula9/entity/DisciplinaEntity.java
@@ -0,0 +1,77 @@
+package com.aula.aula9.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "disciplina")
+public class DisciplinaEntity {
+    @Id
+    @Column(name = "id_disciplina")
+    private Integer idDisciplina;
+
+    @Column(name = "nm_disciplina")
+    private String nmDisciplina;
+
+    @Column(name = "nr_carga_horaria")
+    private BigDecimal nrCargaHoraria;
+
+    @ManyToOne
+    @JoinColumn(name = "id_curso", referencedColumnName = "id_curso", nullable = false)
+    private CursoEntity curso;
+
+    public Integer getIdDisciplina() {
+        return idDisciplina;
+    }
+
+    public void setIdDisciplina(Integer idDisciplina) {
+        this.idDisciplina = idDisciplina;
+    }
+
+    public String getNmDisciplina() {
+        return nmDisciplina;
+    }
+
+    public void setNmDisciplina(String nmDisciplina) {
+        this.nmDisciplina = nmDisciplina;
+    }
+
+    public BigDecimal getNrCargaHoraria() {
+        return nrCargaHoraria;
+    }
+
+    public void setNrCargaHoraria(BigDecimal nrCargaHoraria) {
+        this.nrCargaHoraria = nrCargaHoraria;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DisciplinaEntity that = (DisciplinaEntity) o;
+
+        if (idDisciplina != null ? !idDisciplina.equals(that.idDisciplina) : that.idDisciplina != null) return false;
+        if (nmDisciplina != null ? !nmDisciplina.equals(that.nmDisciplina) : that.nmDisciplina != null) return false;
+        if (nrCargaHoraria != null ? !nrCargaHoraria.equals(that.nrCargaHoraria) : that.nrCargaHoraria != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = idDisciplina != null ? idDisciplina.hashCode() : 0;
+        result = 31 * result + (nmDisciplina != null ? nmDisciplina.hashCode() : 0);
+        result = 31 * result + (nrCargaHoraria != null ? nrCargaHoraria.hashCode() : 0);
+        return result;
+    }
+
+    public CursoEntity getCurso() {
+        return curso;
+    }
+
+    public void setCurso(CursoEntity cursoByIdCurso) {
+        this.curso = cursoByIdCurso;
+    }
+}
