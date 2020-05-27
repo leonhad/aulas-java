cd651aa599ed3ff52d99ab4c3a0969e9a880ad32
Leonardo Costa <leonhad@gmail.com>
2020-05-27 19:08:29 -0300

    Adicionado o início do projeto final.

---
+++ SpringWeb/Aula 10/aula10-final/src/main/java/com/aula/aula9/repository/AlunoRepository.java
@@ -0,0 +1,11 @@
+package com.aula.aula9.repository;
+
+import com.aula.aula9.entity.AlunoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AlunoRepository extends JpaRepository<AlunoEntity, Integer> {
+
+    public List<AlunoEntity> findByNomeIgnoreCaseContaining(String nome);
+}
