3fd4f2f0f1ab95cbcdd51fac4a4c79f32d6f231e
Leonardo Costa <leonhad@gmail.com>
2020-08-23 13:31:08 -0300

    Corrigido alguns problemas de mapeamento.

--- SpringWeb/Aula 3/src/main/java/com/example/aula3/Aula03Application.java
+++ SpringWeb/Aula 3/src/main/java/com/example/aula3/Aula03Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Aula3Application {
+public class Aula03Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Aula3Application.class, args);
+        SpringApplication.run(Aula03Application.class, args);
     }
 
 }
--- SpringWeb/Aula 6/src/main/java/com/example/aula6/controller/InicioController.java
+++ SpringWeb/Aula 6/src/main/java/com/example/aula6/controller/InicioController.java
@@ -2,14 +2,14 @@ package com.example.aula6.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InicioController {
 
-    @RequestMapping("/")
+    @GetMapping()
     public String inicio(Model model) {
         model.addAttribute("title", "Hello World!");
         return "index";
